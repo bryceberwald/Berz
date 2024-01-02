f8aa8b541a760ef73ff3d9dce56681c2f359d44b
Bryce Berwald <bryce@berwald.io>
2024-01-01 17:58:03 -0800

    Cleaned up client-side file structure, creating a starting basis for the development of the website.

---
+++ client/src/components/App/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from '../Header/Header';
+import QRCodeBox from '../QRCodeBox/QRCodeBox';
+
+function App() {
+  return (
+    <div className="app-div-container">
+      <Header />
+      <QRCodeBox />
+    </div>
+  );
+};
+
+export default App;
---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import './Header.css';
+
+function Header() {
+  return (
+    <div className="header-container">
+      <h1 className='header-h1'>Website is currently under construction....</h1>
+      <a className="header-a" href="https://www.berz.io/">https://www.berz.io/</a>
+    </div>
+  );
+};
+
+export default Header;
