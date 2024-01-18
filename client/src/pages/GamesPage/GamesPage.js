901cc532293c833f624931cd8a8a5b62e82919b2
Bryce Berwald <bryce@berwald.io>
2024-01-17 20:51:41 -0800

    Added tabs under the game page to allow for sub category options.

--- client/src/pages/GamesPage/GamesPage.js
+++ client/src/pages/GamesPage/GamesPage.js
@@ -1,40 +1,87 @@
-import React from 'react';
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+
 import Navigation from '../../components/Navigation/Navigation';
 import Header from '../../components/Header/Header';
 import './GamesPage.css';
 
-const GamesPage = () => {
-  
-  // Assuming games is an array of Phaser 3 games
-  const games = [
-    // Game components go here
-    // For example: <PhaserGame key={1} />
-    // Add as many games as needed
-  ];
+function CustomTabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+CustomTabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
+export default function BasicTabs() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
 
   return (
     <div className='games-container'>
       <Navigation />
       <Header />
       <br></br>
+
       <div className="top-games-container">
         <h1>Games Page</h1>
       </div>
-      <div className='main-content'>
-          <section>
-            <h3>Currently under construction...</h3>
-          </section>
+      <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', width: '100%' }}>
+        <Box sx={{ borderBottom: 1, borderColor: 'divider', width: '70%' }}>
+          <Tabs value={value} onChange={handleChange} aria-label="basic tabs example" centered>
+            <Tab label="Browser-based games" {...a11yProps(0)} sx={{ fontSize: '0.7rem', color: '#ffffff' }} />
+            <Tab label="Downloadable games" {...a11yProps(1)} sx={{ fontSize: '0.7rem', color: '#ffffff' }} />
+            <Tab label="Extra Information" {...a11yProps(2)} sx={{ fontSize: '0.7rem', color: '#ffffff' }} />
+          </Tabs>
+        </Box>
+        <CustomTabPanel value={value} index={0}>
+          <div className='games-tab-1'>
+            <h1>Browser-based Games</h1>
           </div>
-      <div className='games-wrapper'>
-        {games.map((game, index) => (
-          <div key={index} className='Game'>
-            <h4 className='GameLabel'>Game #{index + 1}</h4>
-            {game}
+        </CustomTabPanel>
+        <CustomTabPanel value={value} index={1}>
+          <div className='games-tab-2'>
+            <h1>Downloadable Games</h1>
           </div>
-        ))}
-      </div>
+        </CustomTabPanel>
+        <CustomTabPanel value={value} index={2}>
+          <div className='games-tab-3'>
+            <h1>Extra Information</h1>
+          </div>
+        </CustomTabPanel>
+      </Box>
     </div>
   );
 };
-
-export default GamesPage;
