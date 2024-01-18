ae2821ee9a7e13c9762c6671b62dc25764ccac0f
Bryce Berwald <bryce@berwald.io>
2024-01-17 18:35:19 -0800

    Changes made to the DiscordPage.

--- client/src/pages/DiscordPage/DiscordPage.js
+++ client/src/pages/DiscordPage/DiscordPage.js
@@ -4,22 +4,25 @@ import Header from '../../components/Header/Header';
 import './DiscordPage.css';
 
 const DiscordPage = () => {
+  const discordImageUrl = 'discord_image.png';
+  const discordInviteUrl = 'https://discord.gg/8GtBmHwuE8';
+
   return (
     <div className="discord-container">
-        
-        <Navigation />
-        <Header />
-        
-        <br></br>
+      <Navigation />
+      <Header />
+      <br></br>
 
-        <div className="top-discord-container">
-            <h1>Discord Page</h1>
-        </div>
-        
-        <div className='main-content'>
-            <h2>Currently under construction...</h2>
-        </div>
+      <div className="top-discord-container">
+        <h1>Discord Page</h1>
+      </div>
 
+      <div className='main-content'>
+        {/* Use the <a> element to make the image clickable */}
+        <a href={discordInviteUrl} target="_blank" rel="noopener noreferrer">
+          <img src={discordImageUrl} alt="Discord" />
+        </a>
+      </div>
     </div>
   );
 };
