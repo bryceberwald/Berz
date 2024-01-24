ff835222676fa9353a4ef6ca59e99a5984f6aecf
Bryce Berwald <bryce@berwald.io>
2024-01-23 19:48:25 -0800

    Modified and cleaned up the Header component.

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -4,18 +4,11 @@ import React from 'react';
 import './Header.css';
 
 const Header = () => {  
-  
-  const discordImageUrl = 'discord_image.png';
-  const discordInviteUrl = 'https://discord.gg/8GtBmHwuE8';
-
   return (
     <header className="header-container">
-      <h1 className="header-h1">Berz</h1>
-      <img src="header_logo.jpeg" alt="Berz Personal Website Image" />
+      <h1>Berz</h1>
       <br></br>
-      <a href={discordInviteUrl} target="_blank" rel="noopener noreferrer">
-        <img className="discord-image" src={discordImageUrl} alt="Discord" />
-      </a>
+      <img src="header_logo.jpeg" alt="Berz Website Image" />
     </header>
   );
 };
